1f06e3fb90c64375f03cfd590eaa49480ea7aa89
Jordan Pagni <jordan@softwebcity.com>
2020-08-06 12:39:37 -0700

    cleaned to match airbnb styleguide

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var app = express();
-const port = 3000;
+const express = require('express');
 
+const app = express();
+const port = 3002;
 
-app.listen(port, () => {
-  console.log(`Now listening on port 3000`);
-});
+app.listen(port);
+console.log('Now listening on port 3000');
