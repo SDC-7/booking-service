b041eef250166cbeaa7bda87251211dde9d7112c
Jordan Pagni <jordan@softwebcity.com>
2020-08-07 16:00:37 -0700

    added seeding algorithm for testing

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { seedData } = require('../database/seedFakeData');
 
 const app = express();
 const port = 3002;
+
+// seedData();
 
 app.listen(port);
 console.log('Now listening on port 3000');
