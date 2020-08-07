9c5c904a95eb832471f3e37e8d1b47a3ca676167
Jordan Pagni <jordan@softwebcity.com>
2020-08-07 16:11:57 -0700

    removed seeding function (to be called via npm script instead)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const { seedData } = require('../database/seedFakeData');
 
 const app = express();
 const port = 3002;
-
-// seedData();
 
 app.listen(port);
 console.log('Now listening on port 3000');
