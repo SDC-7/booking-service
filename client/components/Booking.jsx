8b100c1a5751d06df34097a70352e791f7e1bd4f
Jordan Pagni <jordan@softwebcity.com>
2020-08-11 18:13:22 -0700

    Added functionality on mount and styled components
    Added fetching of current listing id on component mount.  This is based off of the url requested by the user.  Also decided on styled components and tested with a simple main app styled component with inheriting font properties

--- client/components/Booking.jsx
+++ client/components/Booking.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Guests from './Guests.jsx';
 
 class Booking extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      todo: true,
+      listing: {},
+      unavailableDates: {},
     };
   }
 
+  componentDidMount() {
+    const urlId = window.location.href.split('/')[3];
+    this.fetchListingInfo(urlId);
+  }
+
+  fetchListingInfo(urlId) {
+    axios.get(`/api/booking/${urlId}`)
+      .then(({ data }) => {
+        console.log('Listings: ', data.listing);
+        console.log('Unavailable Dates: ', data.unavailableDates);
+        this.setState({
+          listing: data.listing,
+          unavailableDates: data.unavailableDates,
+        });
+      })
+      .catch((err) => {
+        console.error(`Error retrieving listing data from server: ${err}`);
+      });
+  }
+
   render() {
+    const MainApp = styled.div`
+      width: 325px;
+      height: 430px;
+      padding: 24px;
+      border-radius: 12px;
+      box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;
+      font-family: 'Roboto', sans-serif;
+    `;
     return (
-      <h2>This rendered successfully using webpack and babel!</h2>
+      <MainApp>
+        <h2>hello</h2>
+      </MainApp>
     );
   }
 }
