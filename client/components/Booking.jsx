21ee2b26d4f05f90690c63079d5252bda5a0c17d
Jordan Pagni <jordan@softwebcity.com>
2020-08-13 14:56:05 -0700

    added more relevant console logs

--- client/components/Booking.jsx
+++ client/components/Booking.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import styled from 'styled-components';
 // eslint-disable-next-line import/extensions
 import IntroPriceAndRating from './IntroPriceAndRating.jsx';
-import CheckInCheckOutGuests from './CheckInCheckOutGuests.jsx';
+import MainBookingAndButton from './MainBookingAndButton.jsx';
 
 class Booking extends React.Component {
   constructor() {
@@ -24,8 +24,8 @@ class Booking extends React.Component {
   fetchListingInfo(urlId) {
     axios.get(`/api/booking/${urlId}`)
       .then(({ data }) => {
-        console.log('Listings: ', data.listing);
-        console.log('Unavailable Dates: ', data.unavailableDates);
+        console.log('Listings: ', data.listing[0]);
+        console.log('Unavailable Dates: ', data.unavailableDates[0]);
         this.setState({
           listing: data.listing[0],
           unavailableDates: data.unavailableDates[0],
@@ -37,20 +37,24 @@ class Booking extends React.Component {
   }
 
   render() {
+    // NEED to remove margin later (used now for view space)
     const MainApp = styled.div`
       width: 325px;
-      height: 430px;
+      height: auto;
       padding: 24px;
+      margin-left: 400px;
       border: 1px solid rgb(221, 221, 221);
       border-radius: 12px;
       box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;
       font-family: 'Circular Std Book', 'Roboto', sans-serif;
     `;
 
+    const { listing } = this.state;
+
     return (
       <MainApp>
-        <IntroPriceAndRating listing={this.state.listing}/>
-        <CheckInCheckOutGuests />
+        <IntroPriceAndRating listing={listing} />
+        <MainBookingAndButton />
       </MainApp>
     );
   }
