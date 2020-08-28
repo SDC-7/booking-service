b28ad05af8fa52ecde525faf01f49236bd7a837c
Cinzia Borello <cinziaborello@gmail.com>
2020-08-27 21:07:55 -0700

    Complete data generation file

--- dataGeneration.js
+++ dataGeneration.js
@@ -1,9 +1,41 @@
 const faker = require('faker');
+const fs = require('fs');
 
-const host = faker.name.firstName();
-const rating = (Math.random() * (5.00 - 2.00) + 2.00).toFixed(2);
-const raters = faker.random.number({ min: 3, max: 3333 });
-const price = faker.random.number({ min: 70, max: 1700 });
-const discount = faker.random.number({ min: 0, max: 50 });
+const stream = fs.createWriteStream('listings.csv');
 
-console.log(host, rating, raters, price, discount);
+const createOneListing = () => {
+  const host = faker.name.firstName();
+  const rating = (Math.random() * (5.00 - 2.00) + 2.00).toFixed(2);
+  const raters = faker.random.number({ min: 3, max: 3333 });
+  const price = faker.random.number({ min: 70, max: 1700 });
+  const discount = faker.random.number({ min: 0, max: 50 });
+
+  return `${host},${rating},${raters},${price},${discount}\n`;
+}
+
+const startWriting = (writeStream, encoding, done) => {
+  let i = 10000000;
+  function writing() {
+    let canWrite = true;
+    while (i > 0 && canWrite) {
+      i--;
+      let listing = createOneListing();
+
+      if (i === 0){
+        writeStream.write(listing, encoding, done);
+      } else {
+        writeStream.write(listing, encoding);
+      }
+    }
+
+    if(i > 0 && !canWrite) {
+      writeStream.once('drain', writing);
+    }
+  }
+  writing();
+}
+
+stream.write(`host,rating,raters,price,discount\n`, 'utf-8');
+startWriting(stream, 'utf-8', () => {
+  stream.end();
+});
