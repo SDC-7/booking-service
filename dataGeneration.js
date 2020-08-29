4bbfb69ab84a16f01f87ac7593f9ebcfbf9bf694
Cinzia Borello <cinziaborello@gmail.com>
2020-08-29 16:41:24 -0700

    Update data generation script to add an id column

--- dataGeneration.js
+++ dataGeneration.js
@@ -3,23 +3,24 @@ const fs = require('fs');
 
 const stream = fs.createWriteStream('listings.csv');
 
-const createOneListing = () => {
+const createOneListing = (num) => {
+  const id = num;
   const host = faker.name.firstName();
   const rating = (Math.random() * (5.00 - 2.00) + 2.00).toFixed(2);
   const raters = faker.random.number({ min: 3, max: 3333 });
   const price = faker.random.number({ min: 70, max: 1700 });
   const discount = faker.random.number({ min: 0, max: 50 });
 
-  return `${host},${rating},${raters},${price},${discount}\n`;
+  return `${id},${host},${rating},${raters},${price},${discount}\n`;
 };
 
 const startWriting = (writeStream, encoding, done) => {
-  let i = 10000000;
+  let i = 0;
   function writing() {
     const canWrite = true;
-    while (i > 0 && canWrite) {
-      i -= 1;
-      const listing = createOneListing();
+    while (i < 10000000 && canWrite) {
+      i += 1;
+      const listing = createOneListing(i);
 
       if (i === 0) {
         writeStream.write(listing, encoding, done);
@@ -35,7 +36,7 @@ const startWriting = (writeStream, encoding, done) => {
   writing();
 };
 
-stream.write('host,rating,raters,price,discount\n', 'utf-8');
+stream.write('id,host,rating,raters,price,discount\n', 'utf-8');
 startWriting(stream, 'utf-8', () => {
   stream.end();
 });
