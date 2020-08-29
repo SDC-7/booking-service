8652783047ac04ab59f6bc4c70fbd857d0dd49a1
Cinzia Borello <cinziaborello@gmail.com>
2020-08-28 21:07:22 -0700

    Save query to copy listings from csv

--- postgres/postgresLoader.js
+++ postgres/postgresLoader.js
@@ -1,1 +1,3 @@
 INSERT INTO listings (host, rating, raters, price, discount) VALUES ('Cinzia', 3.22, 18, 140, 20);
+
+COPY listings (host, rating, raters, price, discount) FROM '/Users/cinziaborello/hackreactor/SDC-LAirbnb/booking-service/listingsTest.csv' WITH CSV HEADER;
