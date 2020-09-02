1f2e8c1d02e43979f901fd9b5069f3f7fd705b49
Cinzia Borello <cinziaborello@gmail.com>
2020-09-02 16:11:21 -0700

    Update postgres loader file

--- postgres/psqlLoader.js
+++ postgres/psqlLoader.js
@@ -2,7 +2,7 @@ const dbConfig = require('./psqlconfig.js');
 
 (async () => {
   await dbConfig.client.connect();
-  await dbConfig.client.query("COPY listings (host, rating, raters, price, discount) FROM '/Users/cinziaborello/hackreactor/SDC-LAirbnb/booking-service/listings.csv' WITH CSV HEADER;");
+  await dbConfig.client.query("COPY listings (id, host, rating, raters, price, discount) FROM '/Users/cinziaborello/hackreactor/SDC-LAirbnb/booking-service/listings.csv' WITH CSV HEADER;");
   console.log('done');
   await dbConfig.client.end();
 })();
