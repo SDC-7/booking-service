deff03c1ade1a3bfce73550467abd882e3b4b685
Cinzia Borello <cinziaborello@gmail.com>
2020-09-03 21:53:30 -0700

    Remove MySQL and Cassandra files

--- cassandra/cqlLoader.js
+++
@@ -1,4 +0,0 @@
-/*  Load the data to Cassandra by logging into your cluster using cqlsh:
- *    USE lairbnbbooking;
- *    COPY listings (id, host, rating, raters, price, discount) FROM '/Users/cinziaborello/hackreactor/SDC-LAirbnb/booking-service/listings.csv' WITH HEADER = TRUE;
-*/
